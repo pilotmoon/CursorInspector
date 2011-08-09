e8912f0ceb4f3e45e2aca24dcd3b719272a5364b
Nick Moore <nick@pilotmoon.com>
2011-08-09 13:55:58 +0100

    Something just about working.

--- CursorInspector/CursorInspectorAppDelegate.h
+++ CursorInspector/CursorInspectorAppDelegate.h
@@ -10,8 +10,14 @@
 
 @interface CursorInspectorAppDelegate : NSObject <NSApplicationDelegate> {
     NSWindow *window;
+    NSTimer *timer;
 }
 
 @property (assign) IBOutlet NSWindow *window;
+@property (assign) IBOutlet NSString *quickHash;
+@property (assign) IBOutlet NSString *superFastHash;
+@property (assign) IBOutlet NSString *dimensions;
+@property (assign) IBOutlet NSImage *image;
+@property (assign) BOOL fnDown;
 
 @end
--- CursorInspector/CursorInspectorAppDelegate.m
+++ CursorInspector/CursorInspectorAppDelegate.m
@@ -7,14 +7,38 @@
 //
 
 #import "CursorInspectorAppDelegate.h"
+#import "NMCursorUtils.h"
 
 @implementation CursorInspectorAppDelegate
 
-@synthesize window;
+@synthesize window, quickHash, superFastHash, dimensions, fnDown, image;
+
+- (void)timerRoutine
+{
+    if (!self.fnDown) {
+        NSCursor *c=[NSCursor currentSystemCursor];
+        self.quickHash=[[NSNumber numberWithUnsignedInteger:[c quickHash]] stringValue];
+        self.superFastHash=[[NSNumber numberWithUnsignedInteger:[c superFastHash]] stringValue];
+        self.image=[c image];
+        self.dimensions=[NSString stringWithFormat:@"%.0f x %.0f", self.image.size.width, self.image.size.height];
+    }
+}
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
+    [NSEvent addGlobalMonitorForEventsMatchingMask:NSFlagsChangedMask handler:^(NSEvent *event){
+        self.fnDown=([event modifierFlags]&NSFunctionKeyMask)!=0;
+        NSLog(@"boo %d", self.fnDown);
+    }];
+     
+    [window setLevel:NSFloatingWindowLevel];
     // Insert code here to initialize your application
+    timer=[NSTimer scheduledTimerWithTimeInterval:0.1 target:self selector:@selector(timerRoutine) userInfo:0 repeats:YES];
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication
+{
+    return YES;
 }
 
 @end
