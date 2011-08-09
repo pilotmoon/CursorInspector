bedec5cdeb866f83e81ab1b9f3eb33ce487050a3
Nick Moore <nick@pilotmoon.com>
2011-08-09 15:32:27 +0100

    files from NMKit are now included as copies

---
+++ CursorInspector/NMSuperFastHash.h
@@ -0,0 +1,13 @@
+//
+//  NMSuperFastHash.h
+//  NMKit
+//
+//  Created by nim305 on 09/08/2011.
+//
+// this is SuperFastHash by Paul Hsieh
+// http://www.azillionmonkeys.com/qed/hash.html
+//
+
+#import <stdint.h>
+
+uint32_t NMSuperFastHash (const char * data, int len);
